a3384ccf27bfe437ee7d15923ec1a5b80804a664
Deepakk2510 <154998133+Deepakk2510@users.noreply.github.com>
2026-05-04 07:27:29 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+" use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) //we are using nodejs , not browser //error ayegi
+
+let name = "Deepak"
+let age = 18
+let isLoggedIn = false
+let state; // undefined
+let temp = null;//temp request kiya lekin mila nhi toh 0 nhi likh skte
+//kyuki 0 bhi temp hota,isliye null
+
+// number -> w to the power 53
+//null -> standalone value
+//undefined -> 
+//symbol->unique
+
+
+// object
+
+console.log(typeof null); //object type
+console.log(typeof undefined);//undefined type
