200af54a2b89872e77c282bbf93daf6b963f17f7
Deepakk2510 <154998133+Deepakk2510@users.noreply.github.com>
2026-05-04 08:08:55 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = "deepak"
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);//type giving number
-console.log(valueInNumber);// value here is NaN i.e. not a number kyuki score pure number nhi tha
+// console.log(typeof valueInNumber);//type giving number
+// console.log(valueInNumber);// value here is NaN i.e. not a number kyuki score pure number nhi tha
 //sirf 33 likha hota toh ans console.log(valueInNumber); iska 33 aata
 
 //"33" -> 33
@@ -16,7 +16,7 @@ console.log(valueInNumber);// value here is NaN i.e. not a number kyuki score pu
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 //for boolean
 //1 -> true; 0 -> false
@@ -27,8 +27,51 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//==================================== Operations ==================================
+
+let value = 3
+let negvalue = -value
+//console.log(negvalue); -3
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " deepak"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log(1 + "2");//12
+// console.log("1" + 2);//12
+// console.log("1" + "2");//12
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2"); //32
+
+// console.log(+true); //1
+// console.log(+true); //error
+// console.log(+""); //0
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
+
+
+
+
 
 
 
